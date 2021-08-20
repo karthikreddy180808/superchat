f39a96e8c45be60c9eb8cbf89b509335bd327b5b
karthikreddy180808 <reddykarthik2001@gmail.com>
2021-08-20 18:06:22 +0530

    added api_keys to firebase initialise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+//firebase stuff
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+//firebase hooks
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD2Td9DuvEUTDaDLf64at6Nje6npyAtChs",
+  authDomain: "realtimechat-ff388.firebaseapp.com",
+  projectId: "realtimechat-ff388",
+  storageBucket: "realtimechat-ff388.appspot.com",
+  messagingSenderId: "482778360988",
+  appId: "1:482778360988:web:c438deae7de0aca4c8d159",
+  measurementId: "G-JZYS31E7CN"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
