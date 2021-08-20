a66caf2df201a78e35f2fa2257d3409cc129fe1a
karthikreddy180808 <reddykarthik2001@gmail.com>
2021-08-20 19:15:53 +0530

    timestap->6:07 <> error not yet solved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 //firebase stuff
@@ -65,15 +65,31 @@ function ChatRoom(){
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
   const [messages] = useCollectionData(query,{idField: 'id'});
+  const [formValue , setFormValue] = useState('');
+
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+    const {uid , photoURL} = auth.currentUser;
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+
+  }
 
   return(
     <>
       <div>
         {messages && messages.map(msg => <ChatMessage key = {msg.id} message = {msg}/>)}
       </div>
-      <form>
-        <input/>
-        <button type"submit">Send</button>
+      <form onSubmit = {sendMessage}>
+        <input value = {formValue} onChange = { (e) => setFormValue(e.target.value)}/>
+        <button type="submit">Send</button>
       </form>
     </>
   )
